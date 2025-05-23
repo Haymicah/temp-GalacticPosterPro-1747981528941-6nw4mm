96c62ddb78fea623f8721390ddd0016a6d975053
Haymicah <49584133+Haymicah@users.noreply.github.com>
2025-05-23 09:28:01 +0300

    Copy src/pages/admin/AdminDashboard.tsx from GalacticPosterPro

---
+++ src/pages/admin/AdminDashboard.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { AdminDashboard as AdminDashboardComponent } from '../../components/admin/AdminDashboard';
+
+export default function AdminDashboard() {
+  return <AdminDashboardComponent />;
+}
